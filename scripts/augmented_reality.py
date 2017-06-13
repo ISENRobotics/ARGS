6833fa68f4076715cdf55e7edf214f20af6ef00a
gbiannic <gilles.biannic@isen-bretagne.fr>
2017-06-13 15:11:30 +0200

    dirty square

--- scripts/augmented_reality.py
+++ scripts/augmented_reality.py
@@ -37,13 +37,49 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((4*5,3), np.float32)
 objp[:,:2] = np.mgrid[0:5,0:4].T.reshape(-1,2)
 
-axis = np.float32([[3,0,0], [0,3,0], [0,0,0]]).reshape(-1,3)
+axis = np.float32([[1,0,0], [0,1,0], [0,0,1]]).reshape(-1,3)
+
 
 def draw(img, corners, imgpts):
+    lineWidth = 2
+    lineLenght = 6
+    borderColor = (154,18,179)
     corner = tuple(corners[0].ravel())
-    cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 3)
-    cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 3)
-    cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 0)
+
+    cv2.line(img, corner, tuple(imgpts[0].ravel()), borderColor, lineWidth)
+    cv2.line(img, corner, tuple(imgpts[1].ravel()), borderColor, lineWidth)
+
+    corner_x = corner[0]
+    corner_y = corner[1]
+    ptX_x = imgpts[0][0][0]
+    ptX_y = imgpts[0][0][1]
+    ptY_x = imgpts[1][0][0]
+    ptY_y = imgpts[1][0][1]
+
+    vectX = ( ptX_x - corner_x , ptX_y - corner_y )
+    vectX_away = ( lineLenght * vectX[0], lineLenght * vectX[1] )
+    vectY = ( ptY_x - corner_x , ptY_y - corner_y )
+    vectY_away = ( lineLenght * vectY[0], lineLenght * vectY[1] )
+
+    cv2.line(img, corner, ( int( corner_x + vectX_away[0] ), int( corner_y + vectX_away[1] ) ), borderColor, lineWidth)
+
+    cv2.line(img, corner, ( int( corner_x + vectY_away[0] ), int( corner_y + vectY_away[1] ) ), borderColor, lineWidth)
+
+    cv2.line(
+    img,
+    ( int( corner_x + vectY_away[0] ), int( corner_y + vectY_away[1] ) ),
+    ( int( corner_x + vectY_away[0] + vectX_away[0] ), int( corner_y + vectY_away[1] + vectX_away[1] ) ),
+    borderColor,
+    lineWidth)
+
+    cv2.line(
+    img,
+    ( int( corner_x + vectX_away[0] ), int( corner_y + vectX_away[1] ) ),
+    ( int( corner_x + vectY_away[0] + vectX_away[0] ), int( corner_y + vectY_away[1] + vectX_away[1] ) ),
+    borderColor,
+    lineWidth)
+
+    #cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 3)
 
 
 
@@ -76,19 +112,15 @@ class image_feature:
         np_arr = np.fromstring(ros_data.data, np.uint8)
         image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
 
-        # Init
-        time1 = time.time()
         # Arrays to store object points and image points from all the images.
         objpoints = [] # 3d point in real world space
         imgpoints = [] # 2d points in image plane.
-
 
         # convert np image to grayscale
         gray = cv2.cvtColor(image_np,cv2.COLOR_BGR2GRAY)
 
         # Detect pattern (chessboard)
         ret, corners = cv2.findChessboardCorners(gray, (5,4), None) 
-        time2 = time.time()
 
         # If found, add object points, image points (after refining them)
         if ret == True:
