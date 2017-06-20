cbe303bd78049ecb348c3470eaf572bfade06776
vok29 <pierre.yves.mingam@gmail.com>
2017-06-20 10:14:31 +0200

    [pym][IHM] modification IHM ainsi que du README

--- scripts/ihm/index.py
+++ scripts/ihm/index.py
@@ -76,23 +76,24 @@ html = """<!doctype html>
 <section class="main-section" id="service"><!--main-section-start-->
 	<div class="container">
     	<h2>Game</h2>
-    	<h6>We offer exceptional service with complimentary hugs.</h6>
+    	<h6>Let's the game begin<a href="img/saw.jpg">.</a></h6>
         <div class="row">
-        	<div class="col-lg-4 col-sm-6 wow fadeInLeft delay-05s">
-							<iframe width="620" height="480" style="margin:auto"
-										src="http://192.168.2.127:8182/stream?topic=/augmented_reality_output/image_raw&type=ros_compressed">
+        	<div class="col-xs-offset-3 wow fadeInLeft delay-05s">
+							<iframe width="620" height="480"
+										src="http://192.168.2.126:8182/stream?topic=/usb_cam/image_raw&type=ros_compressed">
 										<!--
-										http://192.168.2.127:8182/stream?topic=/usb_cam/image_raw&type=ros_compressed
+                                        http://192.168.2.126:8182/stream?topic=/augmented_reality_output/image_raw&type=ros_compressed
+										http://192.168.2.126:8182/stream?topic=/usb_cam/image_raw&type=ros_compressed
 										_image_transport:=compressed-->
 								</iframe>
             </div>
-						<div>
+            </div>
+						<div class="col-xs-offset-3 wow fadeInLeft delay-05s">
 							<form action="/index.py" method="post">
 						        <input type="text" name="name" value="A1" />
 						        <input type="submit" name="send" value="Envoyer mouvement">
-						    </form>
+						  </form>
 						</div>
-            </div>
         </div>
 	</div>
 </section><!--main-section-end-->
@@ -105,112 +106,55 @@ html = """<!doctype html>
             <div class="team-leader-box">
                 <div class="team-leader wow fadeInDown delay-03s">
                     <div class="team-leader-shadow"><a href="#"></a></div>
-                    <img src="img/team-leader-pic1.jpg" alt="">
+                    <img src="img/gilles.jpg" alt="">
                     <ul>
-                        <li><a href="#" class="fa-twitter"></a></li>
-                        <li><a href="#" class="fa-facebook"></a></li>
-                        <li><a href="#" class="fa-pinterest"></a></li>
-                        <li><a href="#" class="fa-google-plus"></a></li>
+                        <li><a href="https://fr.linkedin.com/in/gilles-biannic-46834b11a" class="fa-linkedin"></a></li>
                     </ul>
                 </div>
-                <h3 class="wow fadeInDown delay-03s">Walter White</h3>
-                <span class="wow fadeInDown delay-03s">Chief Executive Officer</span>
-                <p class="wow fadeInDown delay-03s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat sollicitudin cursus. Dolor sit amet, consectetur adipiscing elit proin consequat.</p>
+                <h3 class="wow fadeInDown delay-03s">Gilles</h3>
+                <span class="wow fadeInDown delay-03s">Seigneur sith</span>
+                <p class="wow fadeInDown delay-03s">"Wololo !" Citation d'un prêtre</p>
             </div>
             <div class="team-leader-box">
                 <div class="team-leader  wow fadeInDown delay-06s">
                     <div class="team-leader-shadow"><a href="#"></a></div>
-                    <img src="img/team-leader-pic2.jpg" alt="">
+                    <img src="img/david2.jpg" alt="">
                     <ul>
-                        <li><a href="#" class="fa-twitter"></a></li>
-                        <li><a href="#" class="fa-facebook"></a></li>
-                        <li><a href="#" class="fa-pinterest"></a></li>
-                        <li><a href="#" class="fa-google-plus"></a></li>
+                        <li><a href="https://fr.linkedin.com/in/david-floc-h-510797115" class="fa-linkedin"></a></li>
                     </ul>
                 </div>
-                <h3 class="wow fadeInDown delay-06s">Jesse Pinkman</h3>
-                <span class="wow fadeInDown delay-06s">Product Manager</span>
-                <p class="wow fadeInDown delay-06s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat sollicitudin cursus. Dolor sit amet, consectetur adipiscing elit proin consequat.</p>
+                <h3 class="wow fadeInDown delay-06s">David</h3>
+                <span class="wow fadeInDown delay-06s">Sergent chef</span>
+                <p class="wow fadeInDown delay-06s">"Un deux un deux, ennemis repérés !" Citation Droïde de combat B241 </p>
             </div>
+						<div class="team-leader-box">
+								<div class="team-leader wow fadeInDown delay-09s">
+										<div class="team-leader-shadow"><a href="#"></a></div>
+										<img src="img/pym.jpg" alt="">
+										<ul>
+												<li><a href="https://fr.linkedin.com/in/pierre-yves-mingam-72196a10a" class="fa-linkedin"></a></li>
+										</ul>
+								</div>
+								<h3 class="wow fadeInDown delay-09s">Pierre-yves</h3>
+								<span class="wow fadeInDown delay-09s">Contrebandier de bâtons de la mort</span>
+								<p class="wow fadeInDown delay-06s">"Tu aime l'odeur du bruit de l'eau ?"</p>
+						</div>
             <div class="team-leader-box">
                 <div class="team-leader wow fadeInDown delay-09s">
                     <div class="team-leader-shadow"><a href="#"></a></div>
-                    <img src="img/team-leader-pic3.jpg" alt="">
+                    <img src="img/matthieu2.jpg" alt="">
                     <ul>
-                        <li><a href="#" class="fa-twitter"></a></li>
-                        <li><a href="#" class="fa-facebook"></a></li>
-                        <li><a href="#" class="fa-pinterest"></a></li>
-                        <li><a href="#" class="fa-google-plus"></a></li>
+                        <li><a href="https://fr.linkedin.com/in/matthieu-philippe-30269082" class="fa-linkedin"></a></li>
                     </ul>
                 </div>
-                <h3 class="wow fadeInDown delay-09s">Skyler white</h3>
-                <span class="wow fadeInDown delay-09s">Accountant</span>
-                <p class="wow fadeInDown delay-09s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat sollicitudin cursus. Dolor sit amet, consectetur adipiscing elit proin consequat.</p>
+                <h3 class="wow fadeInDown delay-09s">Matthieu</h3>
+                <span class="wow fadeInDown delay-09s">kounchita</span>
+                <p class="wow fadeInDown delay-06s">"Un intellectuel assis va moins loin qu'un con qui marche." Citation de Michel Audiard</p>
             </div>
         </div>
     </div>
 </section><!--main-section team-end-->
 
-
-<div class="container">
-	<section class="main-section contact" id="contact">
-
-        <div class="row">
-        	<div class="col-lg-6 col-sm-7 wow fadeInLeft">
-            	<div class="contact-info-box address clearfix">
-                	<h3><i class=" icon-map-marker"></i>Address:</h3>
-                	<span>308 Negra Arroyo Lane<br>Albuquerque, New Mexico, 87111.</span>
-                </div>
-                <div class="contact-info-box phone clearfix">
-                	<h3><i class="fa-phone"></i>Phone:</h3>
-                	<span>1-800-BOO-YAHH</span>
-                </div>
-                <div class="contact-info-box email clearfix">
-                	<h3><i class="fa-pencil"></i>email:</h3>
-                	<span>hello@knightstudios.com</span>
-                </div>
-            	<div class="contact-info-box hours clearfix">
-                	<h3><i class="fa-clock-o"></i>Hours:</h3>
-                	<span><strong>Monday - Thursday:</strong> 10am - 6pm<br><strong>Friday:</strong> People work on Fridays now?<br><strong>Saturday - Sunday:</strong> Best not to ask.</span>
-                </div>
-                <ul class="social-link">
-                	<li class="twitter"><a href="#"><i class="fa-twitter"></i></a></li>
-                    <li class="facebook"><a href="#"><i class="fa-facebook"></i></a></li>
-                    <li class="pinterest"><a href="#"><i class="fa-pinterest"></i></a></li>
-                    <li class="gplus"><a href="#"><i class="fa-google-plus"></i></a></li>
-                    <li class="dribbble"><a href="#"><i class="fa-dribbble"></i></a></li>
-                </ul>
-            </div>
-        	<div class="col-lg-6 col-sm-5 wow fadeInUp delay-05s">
-            	<div class="form">
-
-                    <div id="sendmessage">Your message has been sent. Thank you!</div>
-                    <div id="errormessage"></div>
-                    <form action="" method="post" role="form" class="contactForm">
-                        <div class="form-group">
-                            <input type="text" name="name" class="form-control input-text" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                            <div class="validation"></div>
-                        </div>
-                        <div class="form-group">
-                            <input type="email" class="form-control input-text" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                            <div class="validation"></div>
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control input-text" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                            <div class="validation"></div>
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control input-text text-area" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                            <div class="validation"></div>
-                        </div>
-
-                        <div class="text-center"><button type="submit" class="input-btn">Send Message</button></div>
-                    </form>
-                </div>
-            </div>
-        </div>
-</section>
-</div>
 <footer class="footer">
     <div class="container">
         <div class="footer-logo"><a href="#"><img src="img/footer-logo.png" alt=""></a></div>
