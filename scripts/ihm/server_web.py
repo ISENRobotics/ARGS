363009feda27e431280e81e2f38e1e5a35e6a7c2
vok29 <pierre.yves.mingam@gmail.com>
2017-06-22 11:45:12 +0200

    [pym][IHM] save avancement IHM

--- scripts/ihm/server_web.py
+++ scripts/ihm/server_web.py
@@ -28,7 +28,6 @@ def _server(self):
     handler = CGIHTTPServer.CGIHTTPRequestHandler
     handler.cgi_directories = ["/"]
     print "Serveur actif sur le port :", PORT
-
     httpd = server(server_address, handler)
     httpd.serve_forever()
 
